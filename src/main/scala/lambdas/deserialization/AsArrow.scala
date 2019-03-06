6190999b12dec25de7bd58b9d8f8839ae3a08f7a
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2019-03-06 09:43:21 +0100

    wip

--- src/main/scala/lambdas/deserialization/AsArrow.scala
+++ src/main/scala/lambdas/deserialization/AsArrow.scala
@@ -7,30 +7,40 @@ abstract class AsArrow[A]{
   type T1
   type T2
   val tA: TQ[A]
-  val eq: Option[(TQ[T1], TQ[T2], A === (T1 => T2))]
+  // val eq: Option[(TQ[T1], TQ[T2], A === (T1 => T2))]
 
-  def cast[F[_]](pa: F[A]): Option[(TQ[T1], TQ[T2], F[T1 => T2])] =
-    eq.map{ eq => (eq._1, eq._2, eq._3.subst[F](pa)) }
+  def cast[F[_]](pa: F[A]): Option[(TQ[T1], TQ[T2], F[T1 => T2])]
+    // eq.map{ eq => (eq._1, eq._2, eq._3.subst[F](pa)) }
 }
 
 object AsArrow{
 
-  def unapply[A](asA: AsArrow[A]): Option[(TQ[A], Option[(TQ[asA.T1], TQ[asA.T2], A === (asA.T1 => asA.T2))])] =
-    Some((asA.tA, asA.eq))
+  // def unapply[A](asA: AsArrow[A]): Option[(TQ[A], Option[(TQ[asA.T1], TQ[asA.T2], A === (asA.T1 => asA.T2))])] =
+  //   Some((asA.tA, asA.eq))
 
-  def apply[A, _T1, _T2](_tA: TQ[A],
-      _eq: Option[(TQ[_T1], TQ[_T2], A === (_T1 => _T2))]) = new AsArrow[A]{
-    type T1 = _T1
-    type T2 = _T2
-    val tA = _tA
-    val eq = _eq
-  }
+  // def apply[A, _T1, _T2](_tA: TQ[A],
+  //     _eq: Option[(TQ[_T1], TQ[_T2], A === (_T1 => _T2))]) = new AsArrow[A]{
+  //   type T1 = _T1
+  //   type T2 = _T2
+  //   val tA = _tA
+  //   val eq = _eq
+  // }
 
   implicit val AsArrowTSYM = new TSYM[AsArrow]{
-    def tint: AsArrow[Int] =
-      AsArrow.apply[Int, Nothing, Nothing](TSYM[TQ].tint, None)
+    def tint: AsArrow[Int] = new AsArrow[Int]{
+      type T1 = Nothing
+      type T2 = Nothing
+      val tA = TSYM[TQ].tint
+      def cast[F[_]](pa: F[Int]): Option[(TQ[T1], TQ[T2], F[T1 => T2])] =
+        None
+    }
 
-    def tarr[T1, T2](t1: AsArrow[T1], t2: AsArrow[T2]): AsArrow[T1 => T2] =
-      AsArrow.apply[T1 => T2, T1, T2](t1.tA -> t2.tA, Some((t1.tA, t2.tA, refl)))
+    def tarr[_T1, _T2](t1: AsArrow[_T1], t2: AsArrow[_T2]) = new AsArrow[_T1 => _T2]{
+      type T1 = _T1
+      type T2 = _T2
+      val tA = t1.tA -> t2.tA
+      def cast[F[_]](pa: F[T1 => T2]): Option[(TQ[T1], TQ[T2], F[T1 => T2])] =
+        Some((t1.tA, t2.tA, refl.subst[F](pa)))
+    }
   }
 }
