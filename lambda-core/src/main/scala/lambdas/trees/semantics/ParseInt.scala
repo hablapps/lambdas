1793940e8f0bcb175e124660b51ca0f347f161d7
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2019-03-13 18:01:31 +0100

    Add ParseInt as independent module

--- lambda-core/src/main/scala/lambdas/trees/semantics/ParseInt.scala
+++ lambda-core/src/main/scala/lambdas/trees/semantics/ParseInt.scala
@@ -1,27 +1,27 @@
-// package lambdas
-// package trees
-// package semantics
+package lambdas
+package trees
+package semantics
 
-// import syntax._
-// import safecast._
-// import interpreters._
-// import arithmetic._
+import syntax._
+import safecast._
+import interpreters._
+import arithmetic._
 
-// case class ParseInt[F[_]]()(implicit A: Arithmetic[F])
-//     extends Interpreter[Tree, Either[String, DynTerm[F]]] {
+case class ParseInt[F[_]]()(implicit A: Arithmetic[F])
+    extends Interpreter[Tree, Either[String, DynTerm[F]]] {
 
-//   def apply(tree: Tree) = tree match {
-//     case IntT(i) =>
-//       Right(DynLTerm(tint[TypeTerm], A.int(i)))
+  def apply(tree: Tree) = tree match {
+    case IntT(i) =>
+      Right(DynTerm(tint[TypeTerm], A.int(i)))
 
-//     case Add(e1, e2) =>
-//       for {
-//         dt1 <- apply(e1)
-//         dt2 <- apply(e2)
-//         _dt1 <- dt1
-//           .as(tint[TypeTerm])
-//           .toEither(s"First operand of add, not an integer: ${dt1.typ}")
-//         _dt2 <- dt2.asInt.toEither(s"Second operand of add, not an integer: ${dt2.typ}")
-//       } yield DynLTerm(tint[TypeTerm], A.add(_dt1, _dt2))
-//   }
-// }
+    case Add(e1, e2) =>
+      for {
+        dt1 <- apply(e1)
+        dt2 <- apply(e2)
+        _dt1 <- dt1
+          .as(tint[TypeTerm])
+          .toEither(s"First operand of add, not an integer: ${dt1.typ}")
+        _dt2 <- dt2.asInt.toEither(s"Second operand of add, not an integer: ${dt2.typ}")
+      } yield DynTerm(tint[TypeTerm], A.add(_dt1)(_dt2))
+  }
+}
