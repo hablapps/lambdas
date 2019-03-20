8c01f83b86c9d909ccffbae649e0967ceccb987e
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2019-03-20 13:17:52 +0100

    wip

--- lambda-core/src/main/scala/lambdas/trees/semantics2/Gamma.scala
+++ lambda-core/src/main/scala/lambdas/trees/semantics2/Gamma.scala
@@ -4,37 +4,39 @@ package semantics2
 
 import tfdb._, syntax._
 
-trait Gamma[Γ, E] {
-  def findVar[T[_], P[_, _]: Lambda](name: String, gamma: Γ): Either[String, DynLTerm[T, P, E]]
+trait Gamma[Γ, E, T[_]] {
+  def findVar[P[_, _]: Lambda](name: String, gamma: Γ): Either[String, DynLTerm[T, P, E]]
 }
 
 object Gamma {
 
   case class Var[T[_], A](name: String, typ: T[A])
 
-  implicit val notFound: Gamma[Unit, Unit] = new Gamma[Unit, Unit] {
-    def findVar[T[_], P[_, _]: Lambda](
+  implicit def notFound[T[_]]: Gamma[Unit, Unit, T] = new Gamma[Unit, Unit, T] {
+    def findVar[P[_, _]: Lambda](
         name: String,
         gamma: Unit
     ): Either[String, DynLTerm[T, P, Unit]] =
       Left(s"Var not found: $name")
   }
 
-  implicit def foundVar[Γ, E, A](implicit G: Gamma[Γ, E]): Gamma[(Var[T, A], Γ), (A, E)] =
-    new Gamma[(Var[T, A], Γ), (A, E)] {
+  implicit def foundVar[Γ, E, T[_], A](
+      implicit G: Gamma[Γ, E, T]
+  ): Gamma[(Var[T, A], Γ), (A, E), T] =
+    new Gamma[(Var[T, A], Γ), (A, E), T] {
 
-      def findVar[T[_], A, P[_, _]: Lambda](
+      def findVar[P[_, _]: Lambda](
           name: String,
           gamma: (Var[T, A], Γ)
       ): Either[String, DynLTerm[T, P, (A, E)]] =
         gamma match {
 
           case (Var(_, typ), _) =>
-            Right(DynLTerm(typ, vz[P, E, T]))
+            Right(DynLTerm(typ, vz[P, E, A]))
 
           case (_, tail) =>
             for {
-              dt <- G.findVar(name, tail).right
+              dt <- G.findVar[P](name, tail).right
             } yield DynLTerm(dt.typ, vs(dt.term))
         }
     }
