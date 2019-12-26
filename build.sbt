a247db614df15fefdc3776ac093f70f61f5ce902
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2019-12-26 02:46:33 +0100

    remove dep.

--- build.sbt
+++ build.sbt
@@ -24,10 +24,7 @@ lazy val `lambda-core` = project
     libraryDependencies ++= Seq(
       "com.chuusai" %% "shapeless" % "2.3.3",
       "org.scalatest" %% "scalatest" % "3.0.0",
-      "org.typelevel" %% "cats-core" % "1.6.0",
-      "io.circe" %% "circe-core"    % "0.11.1",
-      "io.circe" %% "circe-parser"  % "0.11.1",
-      "io.circe" %% "circe-generic" % "0.11.1"
+      "org.typelevel" %% "cats-core" % "1.6.0"
     ),
     // scalacOptions ++= Seq(
     //   "-Ywarn-unused-import",
--- lambda-core/src/main/scala/lambdas/trees/Tree.scala
+++ lambda-core/src/main/scala/lambdas/trees/Tree.scala
@@ -1,38 +1,6 @@
 package lambdas
 package trees
 
-import cats.implicits._
-import io.circe.{ Decoder, DecodingFailure, Encoder, Json }
-import io.circe.syntax._
-
 sealed abstract class Tree
 case class Leaf(l: String)                extends Tree
 case class Node(r: String, c: List[Tree]) extends Tree
-
-object Tree {
-  implicit val CirceTreeEncoder: Encoder[Tree] =
-    Encoder.instance {
-      case Node(r, trs) => Json.obj(r -> trs.asJson)
-      case Leaf(l)      => Json.fromString(l)
-    }
-
-  implicit val CirceLeafDecoder: Decoder[Leaf] =
-    Decoder[String].map(Leaf)
-
-  implicit val CirceNodeDecoder: Decoder[Node] =
-    Decoder.instance { c =>
-      val json = c.value
-      (for {
-        jsonObject <- json.asObject
-        h          <- jsonObject.keys.headOption
-        v          <- jsonObject.values.headOption
-        trees = v.as[List[Tree]]
-        node  = trees.map(Node(h, _))
-      } yield node).fold(
-        DecodingFailure("Expected `object` with `array` value", c.history).asLeft[Node]
-      )(identity)
-    }
-
-  implicit val CirceTreeDecoder: Decoder[Tree] =
-    List[Decoder[Tree]](Decoder[Leaf].widen, Decoder[Node].widen).reduceLeft(_ or _)
-}
